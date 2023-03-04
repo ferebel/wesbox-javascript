9f13e699ef59bbdc4d529f015b29317530460018
ferebel <rebelatofelipe@gmail.com>
2023-03-04 19:02:41 +0000

    add some stuff

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+
+let bill = 100;
+const taxRate = 0.13;
+
+function calculateBill() {
+    const total = 100 * 1.13;
+
+    return total;
+}
+
+const myTotal = calculateBill();
+console.log(`Your Total is $${myTotal}`);
---
+++ playground/types.js
@@ -0,0 +1,11 @@
+let isDrawing = false;
+
+let age = 18;
+
+const ofAge = age > 19;
+
+console.log(ofAge);
+
+age = 100;
+
+let age2 = 100;
